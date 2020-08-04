f7f8d15f2309b3d88830ea6e28f70357983c4c70
Udhav Gupta <47187515+SimplyDifficult@users.noreply.github.com>
2020-08-04 17:19:52 +0530

    Create 1. Two Sum.cpp

---
+++ Array/1. Two Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            int req=target-nums[i];
+            if(mp.find(req)!=mp.end()){
+                ans.push_back(i);
+                ans.push_back(mp[req]);
+                return ans;
+            }
+            mp[nums[i]]=i;
+        }
+        return {-1,-1};
+    }
+};
