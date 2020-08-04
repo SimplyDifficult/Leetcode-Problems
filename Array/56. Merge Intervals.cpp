da1c0344ccf02bbcb6f42e6ef047168c4becf2a3
Udhav Gupta <47187515+SimplyDifficult@users.noreply.github.com>
2020-08-04 22:53:18 +0530

    Create 56. Merge Intervals.cpp

---
+++ Array/56. Merge Intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    static bool compare(vector<int>a, vector<int>b){
+        return a[0]<b[0];
+    }
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.empty())return vector<vector<int>>{};
+        sort(intervals.begin(),intervals.end(),compare);
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(ans.back()[1]>=intervals[i][0])ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            else ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+};
