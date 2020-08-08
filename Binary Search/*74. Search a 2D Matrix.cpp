93b39d343f26d1510f057bdb4d3208a7a80ba6a2
Udhav Gupta <47187515+SimplyDifficult@users.noreply.github.com>
2020-08-08 11:28:41 +0530

    Update *74. Search a 2D Matrix.cpp

--- Binary Search/*74. Search a 2D Matrix.cpp
+++ Binary Search/*74. Search a 2D Matrix.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row=matrix.size()-1,col=0;
-        while(row>=0 && col<matrix[0].size()){
-            if(matrix[row][col]==target)return true;
-            else if(matrix[row][col]>target)row--;
-            else col++;
+        int r=matrix.size();
+        if(r==0)return false;
+        int c=matrix[0].size();
+        int i=0,j=c-1;
+        while(i<r && j>=0){
+        	if(matrix[i][j]==target)return true;
+        	else if(matrix[i][j]<target)i++;
+        	else j--;
         }
         return false;
     }
