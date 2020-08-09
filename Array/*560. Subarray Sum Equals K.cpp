921376c59441870de7a621c109a4d4fce76d7724
Udhav Gupta <47187515+SimplyDifficult@users.noreply.github.com>
2020-08-09 19:44:42 +0530

    Create *560. Subarray Sum Equals K.cpp

---
+++ Array/*560. Subarray Sum Equals K.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int,int>mp;
+        int cnt=0,sum=0;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp.find(sum-k)!=mp.end())cnt+=mp[sum-k];
+            if(mp.find(sum)!=mp.end())mp[sum]++;
+            else mp[sum]=1;
+        }
+        return cnt;
+    }
+};
