29ba3fdf93d7dcb74c52bbdb196cb138912fcd97
Udhav Gupta <47187515+SimplyDifficult@users.noreply.github.com>
2020-08-11 18:55:58 +0530

    Update *300. Longest Increasing Subsequence.cpp

--- Dynamic P/*300. Longest Increasing Subsequence.cpp
+++ Dynamic P/*300. Longest Increasing Subsequence.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>dp(nums.size());
-        int len=0;
-        for(auto x:nums){
-            int left=0,right=len;
-            while(left!=right){
-                int mid=(left+right)/2;
-                if(dp[mid]<x)left=mid+1;
-                else right=mid;
+        int n=nums.size();
+        if(n==0)return 0;
+        vector<int>dp(n,1);
+        int ans=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i])dp[i]=max(dp[i],1+dp[j]);
             }
-            dp[left]=x;
-            if(left==len)len++;
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
+
+/*class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        int len=0;
+        vector<int>arr(n+1,0);
+        for(int i=0;i<n;i++){
+            int l=0,r=len;
+            while(l<r){
+                int mid=(l+r)/2;
+                if(arr[mid]>=nums[i])r=mid;
+                else l=mid+1;
+            }
+            arr[l]=nums[i];
+            if(l==len)len++;
         }
         return len;
     }
