130d93d8615215738fd60571aa0dbc18ee34456e
Udhav Gupta <47187515+SimplyDifficult@users.noreply.github.com>
2020-08-11 15:18:06 +0530

    Update *518. Coin Change 2.cpp

--- Dynamic P/*518. Coin Change 2.cpp
+++ Dynamic P/*518. Coin Change 2.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        int dp[coins.size()+1][amount+1];
+        for(int i=0;i<=coins.size();i++){
+            for(int j=0;j<=amount;j++)dp[i][j]=0;
+        }
+        dp[0][0]=1;
+        for(int i=1;i<=coins.size();i++){
+            dp[i][0]=1;
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]>j)dp[i][j]=dp[i-1][j];
+                else dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+            }
+        }
+        return dp[coins.size()][amount];
+    }
+};
+
+/*class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
         int ans=0;
         vector<int>dp(amount+1,0);
         dp[0]=1;
