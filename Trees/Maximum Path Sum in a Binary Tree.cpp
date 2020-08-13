f431e356d9a67cc7dd1c1f8c887e877765798fef
Udhav Gupta <47187515+SimplyDifficult@users.noreply.github.com>
2020-08-13 08:44:48 +0530

    Create Maximum Path Sum in a Binary Tree.cpp

---
+++ Trees/Maximum Path Sum in a Binary Tree.cpp
@@ -0,0 +1,33 @@
+For each node there can be four ways that the max path goes through the node:
+1. Node only
+2. Max path through Left Child + Node
+3. Max path through Right Child + Node
+4. Max path through Left Child + Node + Max path through Right Child
+
+
+// This function returns overall maximum path sum in 'res' 
+// And returns max path sum going through root. 
+int findMaxUtil(Node* root, int &res) 
+{ 
+    //Base Case 
+    if (root == NULL) 
+        return 0; 
+  
+    // l and r store maximum path sum going through left and 
+    // right child of root respectively 
+    int l = findMaxUtil(root->left,res); 
+    int r = findMaxUtil(root->right,res); 
+  
+    // Max path for parent call of root. This path must 
+    // include at-most one child of root 
+    int max_single = max(max(l, r) + root->data, root->data); 
+  
+    // Max Top represents the sum when the Node under 
+    // consideration is the root of the maxsum path and no 
+    // ancestors of root are there in max sum path 
+    int max_top = max(max_single, l + r + root->data); 
+  
+    res = max(res, max_top); // Store the Maximum Result. 
+  
+    return max_single; 
+} 
