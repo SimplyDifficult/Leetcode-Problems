a5bb449e488862f72ce6a22504923e8e9fb4a35f
Udhav Gupta <47187515+SimplyDifficult@users.noreply.github.com>
2020-08-18 10:31:46 +0530

    Create *406. Queue Reconstruction by Height.cpp

---
+++ Greedy/*406. Queue Reconstruction by Height.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+    static bool compare(vector<int>a,vector<int>b){
+        return ((a[0]>b[0]) || (a[0]==b[0] && a[1]<b[1]));
+    }
+    
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(),people.end(),compare);
+        vector<vector<int>>ans;
+        for(auto p:people){
+            ans.insert(ans.begin()+p[1],p);
+        }
+        return ans;
+    }
+};
